8bcfe9546e2f959cc90dd02706371bff2a1e539d
kirankatiyar <kirankatiyar16@gmail.com>
2023-04-15 17:11:48 +0530

    Added Inheritance (IS-A Relationship) example

---
+++ src/OOPs_Concept/Inheritance/ElectronicItem.java
@@ -0,0 +1,36 @@
+// Inheritance .....Is a relationship
+package OOPs_Concept.Inheritance;
+public class ElectronicItem {
+    void display(){
+        System.out.println("it is a electronic item");
+    }
+    void show(){
+        System.out.println("it work by electricity");
+    }
+    void useful(){
+        System.out.println("its soo useful");
+    }
+}
+class Refrigerator extends ElectronicItem {
+    String cnm;
+    int p;
+
+    void work() {
+        System.out.println("Refrigerator company name :" + "...." + cnm);
+        System.out.println("LG Refrigerator price:" + "...." + p);
+        System.out.println("Refrigerator keep fruits and food cool ");
+    }
+}
+class NewMain {
+    public static void main(String[] args) {
+        Refrigerator r = new Refrigerator();
+        System.out.println("1st item ****************");
+        System.out.println("Refrigerator");
+        r.cnm = "LG";
+        r.p = 23000;
+        r.work();
+        r.display();
+        r.show();
+        r.useful();
+    }
+}
