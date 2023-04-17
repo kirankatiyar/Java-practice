efee7fefcc3bb0836d69422f3a42ddf144a2a545
kirankatiyar <kirankatiyar16@gmail.com>
2023-04-17 21:33:00 +0530

    Added example of One Dimentional Array

---
+++ src/OOPs_Concept/Array/ArrayTest.java
@@ -0,0 +1,14 @@
+package OOPs_Concept.Array;
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] number = {78,2,58,45,9};
+        System.out.println("given integer is present or not in array");
+        int a=45;
+        for(int integer : number){
+            if(integer==a) {
+            }
+            break;
+        }
+        System.out.println("present");
+    }
+}
